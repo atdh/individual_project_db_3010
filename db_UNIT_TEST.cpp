622b7c48a0da5e8a5101f8a2331e1cebb5eac322
atdh <37274998+atdh@users.noreply.github.com>
2020-10-04 23:10:50 -0600

    Create db_UNIT_TEST.cpp
    unit testing for database class

---
+++ db_UNIT_TEST.cpp
@@ -0,0 +1,15 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "database.h"
+
+
+Database d();
+
+TEST_CASE("Constructor", "[database]")
+{
+  SECTION("Constructor")
+  {
+    //REQUIRE();
+    //REQUIRE();
+  }
+}
